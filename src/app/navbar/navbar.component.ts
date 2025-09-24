7799b1392dc7a271140acb9e6737b8bfd90ef783
Sebastian Giulietti <32080740+giuliettiseba@users.noreply.github.com>
2025-09-24 11:37:36 -0600

    feat: update navbar component with improved layout and cart item count display

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,12 +2,12 @@ import {Component} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import { CartStoreService } from '../cart/cart.store';
 import { Observable } from 'rxjs';
-import {AsyncPipe, NgClass} from '@angular/common';
+import {AsyncPipe, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule, NgClass, AsyncPipe],
+  imports: [RouterModule, AsyncPipe, NgIf],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
